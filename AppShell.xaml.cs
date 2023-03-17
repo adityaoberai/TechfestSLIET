6671ed7104ee8ef204e5f01e8f3cabf2f4b6e2f9
adityaoberai <adityaoberai1@gmail.com>
2023-03-17 22:32:55 +0530

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TechfestSLIET;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace TechfestSLIET;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count+=5;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+    private void ColorBtn_Clicked(object sender, EventArgs e)
+    {
+		ColorBtn.BackgroundColor = Color.FromArgb("#f02e65");
+    }
+}
+
